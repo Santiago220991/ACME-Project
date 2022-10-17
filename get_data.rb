eb1aad1a787cf0cea2446b837a15c331a1418208
Santiago <alexander.cardenas10@outlook.com>
2022-10-17 13:53:37 -0500

    Check linters ready to pull request

--- get_data.rb
+++ get_data.rb
@@ -1,17 +1,17 @@
 class Data
   def read_txt(file)
     @raw_data = []
-    @data=[]
+    @data = []
 
     File.open(file).each do |line|
       @raw_data << line.chomp
-      end
+    end
 
     @raw_data.each do |element|
-        person_schedule = element.split('=')
-        name=person_schedule[0]
-        schedule = person_schedule[1].split(',')
-        @data << {name: name, schedule: schedule} 
+      person_schedule = element.split('=')
+      name = person_schedule[0]
+      schedule = person_schedule[1].split(',')
+      @data << { name: name, schedule: schedule }
     end
     @data
   end
